37c8d4c69e23a2bbba8a5b24c2a3e3c06fb79a16
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2026-05-18 19:39:58 +0800

    Homework 38 commit

---
+++ Homework 38.py
@@ -0,0 +1,9 @@
+# Take two integers from the user
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+
+# Find the product
+product = num1 * num2
+
+# Display the result
+print("The product is:", product)
